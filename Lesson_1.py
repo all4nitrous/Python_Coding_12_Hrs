bf578fc7800eb0451e3cc5432ae566ee4c6cb888
all4nitrous <all4nitrous@gmail.com>
2025-01-31 20:48:33 -0500

    First 2 Lessons completed

---
+++ Lesson_1.py
@@ -0,0 +1,3 @@
+#This is the first Lesson from Bro_Code
+print("I like Pizza")
+print("It's really good!")
---
+++ Lesson_2.py
@@ -0,0 +1,35 @@
+#strings
+first_name = "Nicholas"
+food = "pizza"
+email = "bro123@fake.com"
+
+#Integers
+age = 25
+quanity = 3
+num_of_students = 30
+
+# Float
+price = 10.99
+gpa = 3.2
+
+# Boolean
+is_student = True
+for_sale = False
+
+
+print(f"Hello {first_name}")
+print(f"You like {food}")
+print(f"Your email is: {email}")
+print(f"You are {age} years old")
+print(f"You are buying {quanity} apples")
+print(f"Your class has {num_of_students} students")
+print(f"The price is ${price}")
+print(f"Your gpa is: {gpa}")
+if is_student:
+    print("You are a student")
+else:
+    print("You are not a student")
+if for_sale:
+    print("The car is for sale")
+else:
+    print("The car is not for sale")
