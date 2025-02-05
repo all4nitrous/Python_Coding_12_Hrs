d86b2c27668a7b976b24c16dbbaab699505a0831
all4nitrous <all4nitrous@gmail.com>
2025-02-04 22:46:38 -0500

    Lesson 6 added and started

---
+++ Lesson_6.py
@@ -0,0 +1,26 @@
+#Arithmetic Operators
+
+friends = 10
+
+#friends += 1
+#friends -= 2
+#friends *= 3
+#friends /= 2
+#friends **= 2
+remainder = friends % 3
+
+print(friends)
+
+print("*****************")
+
+x = 3.14
+y = 4
+z = 5
+
+#result = round(x)
+#result = abs(y) #Distance to 0
+#result = pow(4, 3)
+#result = max(x, y, z)
+result = min(x, y, z)
+
+print(result)
